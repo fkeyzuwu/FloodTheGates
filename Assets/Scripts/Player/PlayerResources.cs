a6119600dea82a8615737e93def9abaa71a6e640
fkeyzuwu <fkeyzuwu@gmail.com>
2021-09-20 11:55:16 +0300

    more stuff
    not tested yet

--- Assets/Scripts/Player/PlayerResources.cs
+++ Assets/Scripts/Player/PlayerResources.cs
@@ -5,14 +5,40 @@ using Mirror;
 
 public class PlayerResources : NetworkBehaviour
 {
-    private SyncDictionary<string, int> resources = new SyncDictionary<string, int>();
+    public SyncDictionary<string, int> Resources = new SyncDictionary<string, int>();
     [SerializeField] private Resource[] resourcePrefabs;
 
     void Awake()
     {
         foreach(Resource resource in resourcePrefabs)
         {
-            resources.Add(resource.Name, 0);
+            Resources.Add(resource.Name, 0);
+        }
+    }
+
+    public void AddResource(string name, int amount)
+    {
+        CmdAddResource(name, amount);
+    }
+
+    [Command]
+    private void CmdAddResource(string name, int amount)
+    {
+        Resources[name] += amount;
+    }
+
+    public void SubtractResource(string name, int amount)
+    {
+        CmdSubtractResource(name, amount);
+    }
+
+    [Command]
+    private void CmdSubtractResource(string name, int amount)
+    {
+        Resources[name] -= amount;
+        if(Resources[name] < 0)
+        {
+            Resources[name] = 0;
         }
     }
 }
